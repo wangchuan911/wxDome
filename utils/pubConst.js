6549f23d62fc70cb2442c72e503a1c70c9bf883a
Wang ZhiDong <6027021846@iwhalecloud.com>
2019-10-08 20:56:14 +0800

    demo1

--- utils/pubConst.js
+++ utils/pubConst.js
@@ -1,25 +1,48 @@
 
 module.exports = {
-    stepNomal: [
-        {
-            state:0,
-            name:"订单受理中",
-            desc:"正在为您指派服务人员"
-        },
-        {
-            state:1,
-            name:"派遣服务人员",
-            desc:"正在派遣服务人员"
-        },
-        {
-            state:2,
-            name:"车辆评估",
-            desc:"评估车辆，提供最合适的服务"
-        },
-        {
-            state:3,
-            name:"车辆服务中",
-            desc:"正在为您的车辆提供服务"
-        }
-    ]
+    customer:{step1: [
+            {
+                state:0,
+                name:"订单受理中",
+                desc:"正在为您指派服务人员"
+            },
+            {
+                state:1,
+                name:"派遣服务人员",
+                desc:"正在派遣服务人员"
+            },
+            {
+                state:2,
+                name:"车辆评估",
+                desc:"评估车辆，提供最合适的服务"
+            },
+            {
+                state:3,
+                name:"车辆服务中",
+                desc:"正在为您的车辆提供服务"
+            }
+        ]
+    },
+    worker:{
+        step1:[{
+            desc: "前往目的地"
+            ,fin_desc:"已到达"
+            ,id:0
+        },{
+            desc: "洗前车辆评估"
+            ,fin_desc:"评估完成"
+            ,id:1
+        }, {
+            desc: "开始服务"
+            ,fin_desc:"服务完成"
+            ,id:2
+        },{
+            desc: "服务结果反馈"
+            ,fin_desc:"反馈完成"
+            ,id:3
+        }, {
+            desc: "等待客户验收"
+            ,id:4
+        }]
+    }
 }
