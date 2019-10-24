4ffca4758c1e65ae0e55a4241ad4e1916ffb2c3b
Wang ZhiDong <6027021846@iwhalecloud.com>
2019-10-24 20:23:55 +0800

    demo1

--- pages/index2/index/index.js
+++ pages/index2/index/index.js
@@ -89,6 +89,7 @@ Page({
      */
     onLoad: function (options) {
         const _this = this;
+        this.getUserInfo();
         $Service.login(function (openId) {
             $OrderService.getWorkBum(
                 {custId: openId},
@@ -136,32 +137,7 @@ Page({
                 // const accuracy = res.accuracy
             }
         })
-        if (app.globalData.userInfo) {
-            this.setData({
-                userInfo: app.globalData.userInfo,
-                hasUserInfo: true
-            })
-        } else if (this.data.canIUse) {
-            // 由于 getUserInfo 是网络请求，可能会在 Page.onLoad 之后才返回
-            // 所以此处加入 callback 以防止这种情况
-            app.userInfoReadyCallback = function (res) {
-                this.setData({
-                    userInfo: res.userInfo,
-                    hasUserInfo: true
-                })
-            }
-        } else {
-            // 在没有 open-type=getUserInfo 版本的兼容处理
-            wx.getUserInfo({
-                success: function (res) {
-                    app.globalData.userInfo = res.userInfo
-                    _this.setData({
-                        userInfo: res.userInfo,
-                        hasUserInfo: true
-                    })
-                }
-            })
-        }
+
         for (let idx in this.data.serviceType) {
             const type = this.data.serviceType[idx];
             if (type.checked) {
@@ -220,12 +196,56 @@ Page({
 
     },
     getUserInfo: function (e) {
-        console.log(e)
-        app.globalData.userInfo = e.detail.userInfo
-        this.setData({
-            userInfo: e.detail.userInfo,
-            hasUserInfo: true
-        })
+        const _this = this
+        if (e) {
+            console.log(e)
+            app.globalData.userInfo = e.detail.userInfo
+            this.setData({
+                userInfo: e.detail.userInfo,
+                hasUserInfo: true
+            })
+        } else {
+            if (app.globalData.userInfo) {
+                this.setData({
+                    userInfo: app.globalData.userInfo,
+                    hasUserInfo: true
+                })
+            } else if (this.data.canIUse) {
+                // 由于 getUserInfo 是网络请求，可能会在 Page.onLoad 之后才返回
+                // 所以此处加入 callback 以防止这种情况
+                app.userInfoReadyCallback = function (res) {
+                    _this.setData({
+                        userInfo: res.userInfo,
+                        hasUserInfo: true
+                    })
+                }
+            } else {
+                // 在没有 open-type=getUserInfo 版本的兼容处理
+                wx.getUserInfo({
+                    success: function (res) {
+                        app.globalData.userInfo = res.userInfo
+                        _this.setData({
+                            userInfo: res.userInfo,
+                            hasUserInfo: true
+                        })
+                    }
+                })
+            }
+            wx.getSetting({
+                success: function (res) {
+                    if (res.authSetting['scope.userInfo']) {
+                        wx.getUserInfo({
+                            success: function (res) {
+                                _this.setData({
+                                    userInfo: res.userInfo,
+                                    hasUserInfo: true
+                                })
+                            }
+                        })
+                    }
+                }
+            })
+        }
     },
     /**
      * 用户点击右上角分享
@@ -249,7 +269,8 @@ Page({
         console.info(e)
         this.data.markers[0]
     },
-    bookBut: function () {
+    bookBut: function (e) {
+        this.getUserInfo(e)
         if (this.data.loading.submitBut) return
         const _this = this
         _this.setData({
@@ -275,7 +296,8 @@ Page({
 
         })
     },
-    mileStoneBut: function () {
+    mileStoneBut: function (e) {
+        this.getUserInfo(e)
         const _this = this
         wx.navigateTo({
             url: (_this.data.roleMode == 2) ? '/pages/dispatch/orders/orders' : '/pages/customer/milestone/milestone',
