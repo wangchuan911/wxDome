ffdcf2876dc460a5b631246448f471e751d4af30
Wang ZhiDong <6027021846@iwhalecloud.com>
2019-12-01 20:04:01 +0800

    demo1

--- utils/service/orderService.js
+++ utils/service/orderService.js
@@ -103,16 +103,12 @@ const Methods = {
             vip: data.custLevel ? data.custLevel : "",
             preDate: data.orderArrangeDate ? $Utils.formatTime(new Date(data.orderArrangeDate)) : "",
             carNo: data.carLicenseNumber,
-            carType: "xxx",
-            carColor: "xxxxx",
+            carType: data.carVO.brand || '',
+            carColor: data.carVO.color || '',
             latitude: data.posX,
             longitude: data.posY,
             isDeal: data.orderAppointPerson != null,
-            imgs0: [
-                'https://images.unsplash.com/photo-1551334787-21e6bd3ab135?w=640',
-                'https://images.unsplash.com/photo-1551214012-84f95e060dee?w=640',
-                'https://images.unsplash.com/photo-1551446591-142875a901a1?w=640'
-            ]
+            orderCode: data.orderCode,
         }
         if (data.pictureVOS || [] > 0) {
             let pic;
