17571275ef4711c0e616ce2cfcc959ff8c9a5cfb
Wang ZhiDong <6027021846@iwhalecloud.com>
2019-12-17 09:52:32 +0800

    demo1

--- component/vehicle-panel/index.js
+++ component/vehicle-panel/index.js
@@ -1,60 +1,66 @@
 // component/vehicle-panel/index.js
 Component({
-  externalClasses: ['v-panel'],
-  /**
-   * 组件的属性列表
-   */
-  properties: {
-    isShow: {
-      type: Boolean,
-      value: false,
+    externalClasses: ['v-panel'],
+    /**
+     * 组件的属性列表
+     */
+    properties: {
+        isShow: {
+            type: Boolean,
+            value: false,
+        },
+        buttonBorder: {
+            type: String,
+            value: "1px solid #ccc"
+        },
+        backgroundColor: {
+            type: String,
+            value: "#fff"
+        },
+        //1为省份键盘，其它为英文键盘
+        keyBoardType: {
+            type: Number,
+            value: 1,
+        }
     },
-    buttonBorder: {
-      type: String,
-      value: "1px solid #ccc"
+
+    /**
+     * 组件的初始数据
+     */
+    data: {
+        keyVehicle1: '陕京津沪冀豫云辽',
+        keyVehicle2: '黑湘皖鲁新苏浙赣',
+        keyVehicle3: '鄂桂甘晋蒙吉闽贵',
+        keyVehicle4: '粤川青藏琼宁渝',
+        keyNumber: '1234567890',
+        keyEnInput1: 'QWERTYUIOP',
+        keyEnInput2: 'ASDFGHJKL',
+        keyEnInput3: 'ZXCVBNM',
     },
-    backgroundColor: {
-      type: String,
-      value: "#fff"
-    },
-    //1为省份键盘，其它为英文键盘
-    keyBoardType: {
-      type: Number,
-      value: 1,
+
+    /**
+     * 组件的方法列表
+     */
+    methods: {
+        vehicleTap: function (event) {
+            if (this.data.lock)
+                return;
+            else
+                this.data.lock = true;
+
+            let val = event.target.dataset.value || event.currentTarget.dataset.value;
+            switch (val) {
+                case 'delete':
+                    this.triggerEvent('delete');
+                    this.triggerEvent('inputchange');
+                    break;
+                case 'ok':
+                    this.triggerEvent('ok');
+                    break;
+                default:
+                    this.triggerEvent('inputchange', val);
+            }
+            this.data.lock = false;
+        },
     }
-  },
-
-  /**
-   * 组件的初始数据
-   */
-  data: {
-    keyVehicle1: '陕京津沪冀豫云辽',
-    keyVehicle2: '黑湘皖鲁新苏浙赣',
-    keyVehicle3: '鄂桂甘晋蒙吉闽贵',
-    keyVehicle4: '粤川青藏琼宁渝',
-    keyNumber: '1234567890',
-    keyEnInput1: 'QWERTYUIOP',
-    keyEnInput2: 'ASDFGHJKL',
-    keyEnInput3: 'ZXCVBNM',
-  },
-
-  /**
-   * 组件的方法列表
-   */
-  methods: {
-    vehicleTap: function (event) {
-      let val = event.target.dataset.value||event.currentTarget.dataset.value;
-      switch (val) {
-        case 'delete':
-          this.triggerEvent('delete');
-          this.triggerEvent('inputchange');
-          break;
-        case 'ok':
-          this.triggerEvent('ok');
-          break;
-        default:
-          this.triggerEvent('inputchange', val);
-      }
-    },
-  }
 })
