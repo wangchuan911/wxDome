1c821a9c63e34edde6caa4fdf3be85c196cd0fc4
liang.jiaquan <liang.jiaquan@iwhalecloud.com>
2020-04-01 15:48:21 +0800

    Default Changelist

--- pages/customer/detail/detail.js
+++ pages/customer/detail/detail.js
@@ -62,15 +62,7 @@ Page({
     onReady: function () {
 
     },
-    /*
-    * 下拉刷新
-    * */
-    onPullDownRefresh() {
-        if (this.data.order.orderId) {
-            this.reloadOrder(this.data.order.orderId);
-            wx.stopPullDownRefresh();
-        }
-    },
+
     /**
      * 生命周期函数--监听页面显示
      */
