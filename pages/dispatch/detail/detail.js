0b068ee88affdc97ac0951f752f876e3fcd9b09f
liang.jiaquan <liang.jiaquan@iwhalecloud.com>
2020-04-03 16:06:16 +0800

    工单负责人展示修复

--- pages/dispatch/detail/detail.js
+++ pages/dispatch/detail/detail.js
@@ -115,7 +115,7 @@ Page({
                 // 为指定事件添加一个监听器，获取被打开页面传送到当前页面的数据
                 acceptDataFromOpenedPage: function (data) {
                     console.log(data.data.worker);
-                    order.worker = data.data.worker;
+                    order.worker = data.data.worker.name;
                     _this.setData({
                         order: order
                     })
