0526373887c1557ded7488d2ed0ff803290c84e4
Wang ZhiDong <6027021846@iwhalecloud.com>
2020-04-30 22:55:57 +0800

    demo1

--- pages/dispatch/workers/workers.js
+++ pages/dispatch/workers/workers.js
@@ -21,6 +21,72 @@ Page({
             _this.setData({
                 order: data.data
             })
+            const getTimeLong = (mileSeccend) => {
+                const arr = [[1000, '秒'], [60, '分'], [60, '时'], [24, '天']]
+                let long = (new Date().valueOf() - mileSeccend);
+                let suffix;
+                for (let i = 0; i < arr.length; i++) {
+                    if (long > arr[i][0]) {
+                        long /= arr[i][0];
+                        suffix = arr[i][1];
+                        continue;
+                    }
+                    break;
+                }
+                long = parseInt(long);
+                return {long, suffix}
+            }
+
+            $UserService.getWorkers({orderId: _this.data.order.orderId}, function (res) {
+                _this.setData({
+                    ['workers']: res.data.result
+                })
+                const from = _this.data.order.latitude + ',' + _this.data.order.longitude;
+                (res.data.result || []).forEach((worker, index) => {
+                    if (!worker.workerStatus) return
+                    const key = 'workers[' + index + ']';
+                    _this.setData({
+                        [key + '.orders']: worker.workerStatus.orders,
+                        [key + '.lastPosTime']: getTimeLong(worker.workerStatus.lastPosDate)
+                    })
+                    /*const to = worker.workerStatus.posX + ',' + worker.workerStatus.posY
+                    qqmapsdk.calculateDistance({
+                        mode: "straight",
+                        from,
+                        to,
+                        success: (res) => {
+                            _this.setData({
+                                ['workers[' + index + '].distance']: (res.elements[0] || {}).distance
+                            })
+                        }
+                    })*/
+
+                });
+                const noPos = worker => (worker.workerStatus || {}).posX && (worker.workerStatus || {}).posY;
+                const to = (res.data.result || [])
+                    .filter(noPos)
+                    .map(worker => worker.workerStatus.posX + ',' + worker.workerStatus.posY)
+                    .join(";");
+                qqmapsdk.calculateDistance({
+                    mode: "straight",
+                    from,
+                    to,
+                    success: (res) => {
+                        console.info(res.result.elements);
+                        res.result.elements.forEach((element) => {
+                            _this.data.workers.forEach((worker, index) => {
+                                if (!noPos(worker) || worker.distance
+                                    || (element.to.lat != worker.workerStatus.posX
+                                        && element.to.lng != worker.workerStatus.posY)) return;
+                                _this.setData({
+                                    ['workers[' + index + '].distance']: (element.distance / 1000).toFixed(2)
+                                })
+                            })
+                        });
+
+                    }
+                });
+            })
         })
     },
 
@@ -36,72 +102,7 @@ Page({
      */
     onShow: function () {
         const _this = this;
-        const getTimeLong = (mileSeccend) => {
-            const arr = [[1000, '秒'], [60, '分'], [60, '时'], [24, '天']]
-            let long = (new Date().valueOf() - mileSeccend);
-            let suffix;
-            for (let i = 0; i < arr.length; i++) {
-                if (long > arr[i][0]) {
-                    long /= arr[i][0];
-                    suffix = arr[i][1];
-                    continue;
-                }
-                break;
-            }
-            long = parseInt(long);
-            return {long, suffix}
-        }
 
-        $UserService.getWorkers({orderId: _this.data.order.orderId}, function (res) {
-            _this.setData({
-                ['workers']: res.data.result
-            })
-            const from = _this.data.order.latitude + ',' + _this.data.order.longitude;
-            (res.data.result || []).forEach((worker, index) => {
-                if (!worker.workerStatus) return
-                const key = 'workers[' + index + ']';
-                _this.setData({
-                    [key + '.orders']: worker.workerStatus.orders,
-                    [key + '.lastPosTime']: getTimeLong(worker.workerStatus.lastPosDate)
-                })
-                /*const to = worker.workerStatus.posX + ',' + worker.workerStatus.posY
-                qqmapsdk.calculateDistance({
-                    mode: "straight",
-                    from,
-                    to,
-                    success: (res) => {
-                        _this.setData({
-                            ['workers[' + index + '].distance']: (res.elements[0] || {}).distance
-                        })
-                    }
-                })*/
-
-            });
-            const noPos = worker => (worker.workerStatus || {}).posX && (worker.workerStatus || {}).posY;
-            const to = (res.data.result || [])
-                .filter(noPos)
-                .map(worker => worker.workerStatus.posX + ',' + worker.workerStatus.posY)
-                .join(";");
-            qqmapsdk.calculateDistance({
-                mode: "straight",
-                from,
-                to,
-                success: (res) => {
-                    console.info(res.result.elements);
-                    res.result.elements.forEach((element) => {
-                        _this.data.workers.forEach((worker, index) => {
-                            if (!noPos(worker) || worker.distance
-                                || (element.to.lat != worker.workerStatus.posX
-                                    && element.to.lng != worker.workerStatus.posY)) return;
-                            _this.setData({
-                                ['workers[' + index + '].distance']: (element.distance / 1000).toFixed(2)
-                            })
-                        })
-                    });
-
-                }
-            });
-        })
     },
 
     /**
